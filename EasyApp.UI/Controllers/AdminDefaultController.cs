eab49b8457cbb64adb9f9f797b4c195f8413bdfb
Yusuf Keles <kkelesyusuf23@gmail.com>
2025-01-26 00:01:20 +0300

    Add project files.

---
+++ EasyApp.UI/Controllers/AdminDefaultController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyApp.UI.Controllers
+{
+    public class AdminDefaultController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
